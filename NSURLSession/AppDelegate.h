3cfb771acef95bb7afd9e06d274da33de85235a5
zhuming3834 <zhuming3834@sina.com>
2015-10-28 13:26:16 +0800

    Initial Commit

---
+++ NSURLSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSession
+//
+//  Created by HGDQ on 15/10/28.
+//  Copyright (c) 2015年 HGDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSession
+//
+//  Created by HGDQ on 15/10/28.
+//  Copyright (c) 2015年 HGDQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
